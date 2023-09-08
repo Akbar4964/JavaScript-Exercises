39ca1f8c160440c64095033a21d66ae698f7067e
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-08 10:12:23 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// let a = prompt("Enter a first number:");
+// let b = prompt("Enter a second number:");
+
+// document.write(Number(a) + Number(b));
+
+// const day = prompt("Enter a number from 1 to 7");
+// if (day == 1) {
+//   console.log("Dushanba");
+// } else if (day == 2) {
+//   console.log("Seshanba");
+// } else if (day == 3) {
+//   console.log("Chorshanba");
+// } else if (day == 4) {
+//   console.log("Payshanba");
+// } else if (day == 5) {
+//   console.log("Juma");
+// } else if (day == 6) {
+//   console.log("Shanba");
+// } else if (day == 7) {
+//   console.log("Yakshanba");
+// } else {
+//   console.log("Bunday kun yo'q");
+// }
