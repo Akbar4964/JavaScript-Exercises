e7d5dda1809b9ce8fa7b6114c58cfe32e996522d
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-13 13:03:58 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -90,9 +90,9 @@
 // console.log(minNumber);
 
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === "") {
     alert("You must write something!");
@@ -101,15 +101,15 @@ function newElement() {
   }
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-      var div = this.parentElement;
+      let div = this.parentElement;
       div.style.display = "none";
     };
   }
