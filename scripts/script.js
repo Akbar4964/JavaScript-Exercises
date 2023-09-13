a061d6fb32c3807945c1b854e25c6071de85107e
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-13 11:47:05 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -57,18 +57,18 @@
 // const b = prompt("Enter a number:");
 // const c = prompt("Enter a number:");
 
-function findMinimum(a, b, c) {
-  if (a <= b && a <= c) {
-    return a;
-  } else if (b <= a && b <= c) {
-    return b;
-  } else {
-    return c;
-  }
-}
-const a = 1;
-const b = 3;
-const c = 5;
+// function findMinimum(a, b, c) {
+//   if (a <= b && a <= c) {
+//     return a;
+//   } else if (b <= a && b <= c) {
+//     return b;
+//   } else {
+//     return c;
+//   }
+// }
+// const a = 1;
+// const b = 3;
+// const c = 5;
 
-const min = findMinimum(a, b, c);
-console.log("Eng kichik son: " + min);
+// const min = findMinimum(a, b, c);
+// console.log("Eng kichik son: " + min);
