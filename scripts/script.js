8c9801bbbb90442e8662e5e21c557aa55a0af342
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-14 11:33:19 +0500

    added for of

--- scripts/script.js
+++ scripts/script.js
@@ -327,3 +327,21 @@ function findLargestOddElement(array) {
 }
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 console.log(findLargestOddElement(numbers));
+
+function findLargestOddElement(array) {
+  let largestOdd = 0;
+  let index = 0;
+  for (let i in numbers) {
+    if (array[i] % 2 !== 0 && array[i] > largestOdd) {
+      largestOdd = array[i];
+      index = i;
+    }
+  }
+  if (largestOdd === 0) {
+    return 0;
+  } else {
+    return [largestOdd, index];
+  }
+}
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+console.log(findLargestOddElement(numbers));
