c1613f1de9d300db39c8a7db09ab1547565f42ca
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-14 11:26:10 +0500

    changed codes

--- scripts/script.js
+++ scripts/script.js
@@ -3,24 +3,24 @@ let b = prompt("Enter a second number:");
 
 document.write(Number(a) + Number(b));
 
-const day = prompt("Enter a number from 1 to 7");
-if (day == 1) {
-  console.log("Dushanba");
-} else if (day == 2) {
-  console.log("Seshanba");
-} else if (day == 3) {
-  console.log("Chorshanba");
-} else if (day == 4) {
-  console.log("Payshanba");
-} else if (day == 5) {
-  console.log("Juma");
-} else if (day == 6) {
-  console.log("Shanba");
-} else if (day == 7) {
-  console.log("Yakshanba");
-} else {
-  console.log("Bunday kun yo'q");
-}
+// const day = prompt("Enter a number from 1 to 7");
+// if (day == 1) {
+//   console.log("Dushanba");
+// } else if (day == 2) {
+//   console.log("Seshanba");
+// } else if (day == 3) {
+//   console.log("Chorshanba");
+// } else if (day == 4) {
+//   console.log("Payshanba");
+// } else if (day == 5) {
+//   console.log("Juma");
+// } else if (day == 6) {
+//   console.log("Shanba");
+// } else if (day == 7) {
+//   console.log("Yakshanba");
+// } else {
+//   console.log("Bunday kun yo'q");
+// }
 
 function result(num1, num2, num3) {
   let number = 0;
